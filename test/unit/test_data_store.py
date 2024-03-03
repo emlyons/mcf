546d814cf2ba514f0d7c11705be36a0c15eb5c08
ethan <ethan.lyons@digimarc.com>
2024-03-03 11:57:42 -0800

    added unit tests for data store

---
+++ test/unit/test_data_store.py
@@ -0,0 +1,177 @@
+import unittest
+
+from mcf.data_store.data_store import DataStore
+from mcf.data_store.data_store_status import DataStoreStatus
+
+class TestDataStore(unittest.TestCase):
+	def setUp(self):
+		self.data_store = DataStore()
+
+	def tearDown(self):
+		return
+
+	def test_init(self):
+		self.assertIsNotNone(self.data_store)
+
+	def test_put(self):
+		key = 'key'
+		field = 'field'
+		value = 123
+
+		status = self.data_store.put(key, field, value)
+		
+		self.assertEqual(DataStoreStatus.SUCCESS, status)
+
+	def test_put_collision(self):
+		key = 'key'
+		field = 'field'
+		value1 = 123
+		value2 = 321
+
+		status = self.data_store.put(key, field, value1)
+		status = self.data_store.put(key, field, value2)
+
+		self.assertEqual(DataStoreStatus.FIELD_COLLISION, status)
+
+	def test_put_replace(self):
+		key = 'key'
+		field = 'field'
+		value1 = 123
+		value2 = 321
+
+		status = self.data_store.put(key, field, value1)
+		self.data_store.erase(key, field)
+		status = self.data_store.put(key, field, value2)
+		
+		self.assertEqual(DataStoreStatus.SUCCESS, status)
+
+	def test_put_replace_after_remove(self):
+		key = 'key'
+		field = 'field'
+		value1 = 123
+		value2 = 321
+
+		status = self.data_store.put(key, field, value1)
+		self.data_store.remove(key)
+		status = self.data_store.put(key, field, value2)
+		
+		self.assertEqual(DataStoreStatus.SUCCESS, status)
+
+	def test_get(self):
+		key = 'key'
+		field = 'field'
+		value = 123
+
+		_ = self.data_store.put(key, field, value)
+		status, observed_value = self.data_store.get(key, field)
+		
+		self.assertEqual(DataStoreStatus.SUCCESS, status)
+		self.assertEqual(value, observed_value)
+
+	def test_get_invalid_key(self):
+		key = 'key'
+		field = 'field'
+
+		status, observed_value = self.data_store.get(key, field)
+		
+		self.assertEqual(DataStoreStatus.INVALID_KEY, status)
+
+	def test_get_invalid_field(self):
+		key = 'key'
+		field = 'field'
+		value = 123
+
+		_ = self.data_store.put(key, field, value)
+		status, observed_value = self.data_store.get(key, 'invalid field')
+		
+		self.assertEqual(DataStoreStatus.INVALID_FIELD, status)
+
+	def test_get_collision_replace(self):
+		key = 'key'
+		field = 'field'
+		value1 = 123
+		value2 = 321
+
+		_ = self.data_store.put(key, field, value1)
+		_ = self.data_store.put(key, field, value2)
+		status, observed_value = self.data_store.get(key, field)
+		
+		self.assertEqual(DataStoreStatus.SUCCESS, status)
+		self.assertEqual(value1, observed_value)
+
+	def test_get_replaced(self):
+		key = 'key'
+		field = 'field'
+		value1 = 123
+		value2 = 321
+
+		_ = self.data_store.put(key, field, value1)
+		self.data_store.erase(key, field)
+		_ = self.data_store.put(key, field, value2)
+		status, observed_value = self.data_store.get(key, field)
+		
+		self.assertEqual(DataStoreStatus.SUCCESS, status)
+		self.assertEqual(value2, observed_value)
+
+	def test_get_replaced_after_remove(self):
+		key = 'key'
+		field = 'field'
+		value1 = 123
+		value2 = 321
+
+		_ = self.data_store.put(key, field, value1)
+		self.data_store.remove(key)
+		_ = self.data_store.put(key, field, value2)
+		status, observed_value = self.data_store.get(key, field)
+		
+		self.assertEqual(DataStoreStatus.SUCCESS, status)
+		self.assertEqual(value2, observed_value)
+
+	def test_erase(self):
+		key = 'key'
+		field = 'field'
+		value = 123
+
+		_ = self.data_store.put(key, field, value)
+		status = self.data_store.erase(key, field)
+		
+		self.assertEqual(DataStoreStatus.SUCCESS, status)
+
+	def test_erase_invalid_key(self):
+		key = 'key'
+		field = 'field'
+
+		status = self.data_store.erase(key, field)
+		
+		self.assertEqual(DataStoreStatus.INVALID_KEY, status)
+
+	def test_erase_invalid_field(self):
+		key = 'key'
+		field = 'field'
+		value = 123
+
+		_ = self.data_store.put(key, field, value)
+		status = self.data_store.erase(key, 'invalid field')
+		
+		self.assertEqual(DataStoreStatus.INVALID_FIELD, status)
+
+	def test_remove(self):
+		key = 'key'
+		field = 'field'
+		value = 123
+
+		_ = self.data_store.put(key, field, value)
+		status = self.data_store.remove(key)
+		
+		self.assertEqual(DataStoreStatus.SUCCESS, status)
+
+	def test_remove_invalid_key(self):
+		key = 'key'
+
+		status = self.data_store.remove(key)
+		
+		self.assertEqual(DataStoreStatus.INVALID_KEY, status)
+	
+
+if __name__ == '__main__':
+	unittest.main()
