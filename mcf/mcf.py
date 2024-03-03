8841c18e378f32caa20770221711ca81e3bb0ec0
ethan <ethan.lyons@digimarc.com>
2024-03-03 13:02:51 -0800

    processor configured with constructor

--- mcf/mcf.py
+++ mcf/mcf.py
@@ -3,5 +3,12 @@ from mcf.processor import Processor, ProcessorStatus
 class Api:
 
     def __init__(self):
-        return
+        self.processor = Processor()
+    
+    def add_frame(self, frame):
+        self.processor.process_frame(frame)
+    
+    def next_result(self):
+        return None
+
     
