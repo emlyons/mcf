3a32494da706aab0f0f46917d3e92de2881de1a9
ethan <ethan.lyons@digimarc.com>
2024-03-03 13:01:52 -0800

    processor configured with constructor

--- mcf/processor/processor.py
+++ mcf/processor/processor.py
@@ -2,17 +2,10 @@ from mcf.processor.processor_status import ProcessorStatus
 class Processor:
 
     def __init__(self):
-        self.configured = False
-    
-    def configure(self) -> ProcessorStatus:
-        self.configured = True
-        return ProcessorStatus.ERROR_NOT_IMPLEMENTED
+        return
     
     def process_frame(self, frame) -> ProcessorStatus:
         status = ProcessorStatus.SUCCESS
-
-        if not self.configured:
-            status = ProcessorStatus.ERROR_NOT_CONFIGURED
             
         if status == ProcessorStatus.SUCCESS:
             status = ProcessorStatus.ERROR_NOT_IMPLEMENTED
--- test/unit/test_processor.py
+++ test/unit/test_processor.py
@@ -13,24 +13,11 @@ class TestDataStore(unittest.TestCase):
 	def test_init(self):
 		self.assertIsNotNone(self.processor)
 
-	def test_configure(self):
-		
-		status = self.processor.configure()
-		
-		self.assertEqual(ProcessorStatus.ERROR_NOT_IMPLEMENTED , status)
-
 	def test_process_frame(self):
 
-		_ = self.processor.configure()
 		status = self.processor.process_frame(None)
 		
 		self.assertEqual(ProcessorStatus.ERROR_NOT_IMPLEMENTED , status)
-
-	def test_process_frame_before_configuring(self):
-		
-		status = self.processor.process_frame(None)
-		
-		self.assertEqual(ProcessorStatus.ERROR_NOT_CONFIGURED , status)
 
 if __name__ == '__main__':
 	unittest.main()
