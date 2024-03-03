898a63446bc400b08bf2f7b233d743304c2d24cd
ethan <ethan.lyons@digimarc.com>
2024-03-03 13:20:07 -0800

    video example uses command line args

--- example/example_process_video.py
+++ example/example_process_video.py
@@ -1,26 +1,25 @@
-import numpy as np
+import argparse
 import cv2 as cv
+from mcf import Api as mcf_api
 
-# video meta-data
-# video = cv.VideoCapture(f'/Users/elyons/Documents/dev/repos/motion_compensated_filtering_for_image_recovery/prototype/data/highway.mp4')
-# width = int(video.get(cv.CAP_PROP_FRAME_WIDTH))
-# height = int(video.get(cv.CAP_PROP_FRAME_HEIGHT))
-# frames = int(video.get(cv.CAP_PROP_FRAME_COUNT))
-# fps = int(video.get(cv.CAP_PROP_FPS))
-# print(f'resolution: {width}x{height}, frames: {frames}, fps: {fps}, duration: {frames/fps:.3f} s')
-
-def main():
-    video = cv.VideoCapture(f'/Users/elyons/Documents/dev/repos/motion_compensated_filtering_for_image_recovery/prototype/data/highway.mp4')
+def main(input_path):
+    video = cv.VideoCapture(input_path)
 
     width = int(video.get(cv.CAP_PROP_FRAME_WIDTH))
     height = int(video.get(cv.CAP_PROP_FRAME_HEIGHT))
     frames = int(video.get(cv.CAP_PROP_FRAME_COUNT))
     fps = int(video.get(cv.CAP_PROP_FPS))
 
+    mcf = mcf_api()
+
     while True:
         success, frame = video.read()
 
         if success:
+
+            mcf.add_frame(frame)
+
+            out_frame = mcf.next_result()
 
             cv.imshow(f'{width}x{height}, duration: {frames/fps}s  @ {fps}fps', frame)
             if cv.waitKey(1) & 0xFF == ord("q"):
@@ -31,4 +30,8 @@ def main():
             break
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="My Python Program")
+    parser.add_argument('-i','--input', dest='input', help="path to .mp4 file", required=True)
+    args = parser.parse_args()
+
+    main(args.input)
