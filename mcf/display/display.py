1b5e22a0daf7f7747ea5e2bd686fec01d22e3c80
ethan <ethan.lyons@digimarc.com>
2024-03-09 10:14:37 -0800

    display shows classification confidence

--- mcf/display/display.py
+++ mcf/display/display.py
@@ -1,10 +1,23 @@
 import cv2 as cv
-from mcf.data_types import Frame
+from mcf.data_types import Frame, DetectionRegion
 
 GREEN = (0,255,0)
+RED = (0,0,255)
+PURPLE = (200, 0, 256)
 
-def add_bounding_box(image, detection_region):
+def add_bounding_box(image, detection_region: DetectionRegion):
     top_left, bottom_right = detection_region.bounding_box
+    confidence = detection_region.confidence
+
+    # write confidence on image
+    text = f'{confidence:.4f}'
+    font = cv.FONT_HERSHEY_SIMPLEX
+    font_scale = 2
+    font_thickness = 2
+    text_size = cv.getTextSize(text, font, font_scale, font_thickness)[0]
+    cv.putText(image, text, (top_left[0], top_left[1]), font, font_scale, PURPLE, font_thickness)
+
+    # add bounding box
     cv.rectangle(image, top_left, bottom_right, GREEN, 5)
 
 def add_mask(image, detection_region):
